18095744d6d6e1b47e4a8be4649c377db3dab3d8
davenpi <davenport.ianc@gmail.com>
2025-06-17 12:34:51 -0400

    Scaffolding for client session integration tests

---
+++ tests/client/session/test_session_integration.py
@@ -0,0 +1,31 @@
+from .conftest import BaseSessionTest
+
+
+class TestInitializationFlow(BaseSessionTest):
+    async def test_can_send_requests_after_successful_initialization(self):
+        pass
+
+    async def test_rejects_requests_before_initialization(self):
+        pass
+
+    async def test_handles_initialization_failure_gracefully(self):
+        pass
+
+
+class TestShutdownFlow(BaseSessionTest):
+    async def test_completes_pending_requests_before_shutdown(self):
+        pass
+
+    async def test_rejects_new_requests_during_shutdown(self):
+        pass
+
+    async def test_cleans_up_resources_on_shutdown(self):
+        pass
+
+
+class TestSessionLifecycle(BaseSessionTest):
+    async def test_full_lifecycle_initialization_to_shutdown(self):
+        pass
+
+    async def test_can_restart_after_clean_shutdown(self):
+        pass
